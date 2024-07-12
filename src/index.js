9596273b3d3ee9ca8eb4e3d6af8f069cffcf998a
Shahzaib Qasim <122147898+shaizy384@users.noreply.github.com>
2024-07-12 03:47:18 -0700

    clean code

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Login from './pages/auth/LoginPage';
-import UpdatePasswordPage from './pages/auth/UpdatePassPage/Index';
-import EmailPage from './pages/auth/emailPage/Index';
 import Routers from './routes';
-import Layout from './components/layout';
-import SideBar from './components/layout/SideBar';
 import store from './redux/store';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
@@ -17,13 +12,8 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <>
-        {/* <UpdatePasswordPage/> */}
-        {/* <Login/> */}
-        {/* <EmailPage/> */}
         <ToastContainer />
         <Routers />
-        {/* <Layout/> */}
-        {/* <Layout/> */}
       </>
     </Provider>
   </React.StrictMode>
